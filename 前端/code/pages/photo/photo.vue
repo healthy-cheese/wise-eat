7743d29c1d5b07492fceb20a703e26470a2ec9bc
BH13 <haojl846@nenu.edu.cn>
2020-09-14 17:11:42 +0800

    前端的源码第一次提交

---
+++ 前端/code/pages/photo/photo.vue
@@ -0,0 +1,23 @@
+<!-- 拍照选项tab页 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
