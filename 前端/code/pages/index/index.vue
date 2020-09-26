f2f7f6d0e8b3ad318d133d25246abdf2429b0136
duanfx725 <1257766348@qq.com>
2020-09-26 14:30:56 +0800

    完成静态首页、智食百科、智食圈、个人首页

--- 前端/code/pages/index/index.vue
+++ 前端/code/pages/index/index.vue
@@ -1,9 +1,84 @@
 <!-- 首页 -->
 <template>
+	<view class="">
+		<view class="center-data text-center bg-white data-container">
+			<view class=" padding-lg">
+				<view class="text-lg text-bold text-black text-center text-shadow margin-bottom-sm">
+					本日健康数据
+				</view>
+				<view class="cu-btn line-orange round sm shadow data-button" type="default">
+					查看报表
+					<text class="cuIcon-right lg text-orange"></text>
+				</view>
+				<view class="flex justify-center ">
+				    <circle-percent class="justify-center" bg="#fbb800"  percent="60"></circle-percent>
+				</view>
+			</view>
+			<view class="left-data">
+				<view class="text-xl text-green text-bold text-shadow">
+					323
+				</view>
+				<view class="">
+					摄入(Kcal)
+				</view>
+			</view>
+			<view class="right-data">
+				<view class="text-xl text-blue text-bold text-shadow">
+					231
+				</view>
+				<view class="">
+					消耗(Kcal)
+				</view>
+			</view>
+			<view class="flex justify-around bg-orange padding radius text-center light data-detail">
+				<view class="">
+					<view class="text-lg text-bold">
+						32
+					</view>
+					<view class="">
+						蛋白质(g)
+					</view>
+				</view>
+				<view class="">
+					<view class="text-lg text-bold">
+						382
+					</view>
+					<view class="">
+						碳水化物(g)
+					</view>
+				</view>
+				<view class="">
+					<view class="text-lg text-bold">
+						192
+					</view>
+					<view class="">
+						油脂(g)
+					</view>
+				</view>
+			</view>
+		</view>
+		<!-- 热点文章 -->
+		<view class="margin-top-sm">
+			<view class="bg-white padding-sm solids-bottom" @click="toArticalDetail">
+				<view class="text-bold text-lg">
+					<text>饮食的艺术 ｜ 颠覆你的想象</text>
+				</view>
+				<view class="flex">
+					<view class="margin-right-sm">
+						<image src="../../static/images/tabbar/camara.png" style="width: 100rpx;height: 100rpx;" mode=""></image>
+					</view>
+					<view class="">
+						<text class="text-sm margin-top-sm text-detail">酱料在上桌前淋在饭上。滋滋的响声过后，焦香入味的米粒混着四溢的腊香袭来，这才叫作煲仔，焦香入味的米粒混着四溢的腊香袭来，这才叫作煲仔</text>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
 	
 </template>
 
 <script>
+	import circlePercent from '@/components/circle-percent/circle-percent'
 	export default {
 		data() {
 			
@@ -18,5 +93,43 @@
 </script>
 
 <style>
-	
+	/* 智食数据 */
+	.data-container{
+		background: url(../../static/images/test/bg-fruit.png);
+		background-size: 300rpx,600rpx;
+		
+	}
+	.center-data{
+		position: relative;
+	}
+	.left-data{
+		position: absolute;
+		top:250rpx;
+		left:30rpx;
+		transform: skew(-10deg, 0deg);
+	}
+	.right-data{
+		position: absolute;
+		top:250rpx;
+		right: 30rpx;
+		transform: skew(-10deg, 0deg);
+	}
+	.data-button{
+		position: absolute;
+		top:40rpx;
+		right:50rpx;
+	}
+	.data-detail{
+		opacity: 0.8;
+		border-radius: 20px 20px 0 0;
+	}
+	/* 推荐文章	 */
+	.text-detail{
+		display: -webkit-box;
+		-webkit-box-orient: vertical;
+		-webkit-line-clamp: 2;// 限制快级元素的文本行数
+		overflow: hidden;
+		text-overflow:ellipsis;
+		-o-text-overflow:ellipsis;
+	}
 </style>
--- 前端/code/pages/personal/personal.vue
+++ 前端/code/pages/personal/personal.vue
@@ -1,6 +1,103 @@
 <template>
 	<view>
-		
+		<view class="bg-white padding-lg">
+			<view class="flex">
+				<view class="margin-right-sm">
+					<image class="xl cu-avatar round" src="../../static/images/test/testCat.png" mode=""></image>
+				</view>
+				<view class="">
+					<view class="margin-bottom-sm text-lg text-bold text-black">我只是一只小杰瑞</view>
+					<view class="cu-btn lines-yellow round sm shadow ">
+						编辑资料
+					</view>
+				</view>
+			</view>
+			<view class="flex margin-top-sm text-center">
+				<view class="flex-sub">
+					<view class="text-lg text-bold text-black">
+						0
+					</view>
+					<view class="">
+						关注
+					</view>
+				</view>
+				<view class="flex-sub">
+					<view class="text-lg text-bold text-black">
+						32
+					</view>
+					<view class="">
+						粉丝
+					</view>
+				</view>
+				<view class="flex-sub">
+					<view class="text-lg text-bold text-black">
+						462
+					</view>
+					<view class="">
+						动态
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="text-white bg-gradual-orange padding-sm margin-sm radius shadow shadow-lg">
+			<text class="cuIcon-vip text-bold"></text>
+			全年最低价，会员限时5折！
+		</view>
+		<view class="">
+			<view class="cu-list menu">
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-footprint text-cyan"></text>
+						<text class="text-grey">运动数据</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-list text-green"></text>
+						<text class="text-grey">健康数据</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-circle text-blue"></text>
+						<text class="text-grey">身体测试</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-keyboard text-orange"></text>
+						<text class="text-grey">我的课程</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-edit text-yellow"></text>
+						<text class="text-grey">我的菜谱</text>
+					</view>
+				</view>
+				<view class="cu-item arrow margin-top-sm">
+					<view class="content">
+						<text class="cuIcon-info text-red"></text>
+						<text class="text-grey">帮助与客服</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-circlefill text-purple"></text>
+						<text class="text-grey">关于我们</text>
+					</view>
+				</view>
+				<view class="cu-item arrow">
+					<view class="content">
+						<text class="cuIcon-like text-pink"></text>
+						<text class="text-grey">鼓励一下</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="text-center margin">
+			<button type="default" class="cu-btn lines-yellow round button-hover">退出登陆</button>
+		</view>
 	</view>
 </template>
 
